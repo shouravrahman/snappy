1e230a62315314b845ce4d1235f63f3be1097468
shouravrahman <shouravrahman006@gmail.com>
2023-12-17 21:10:39 +0600

    chore: setup langugaes array

---
+++ utils/utilities.tsx
@@ -0,0 +1,38 @@
+export const languages = [
+	{
+		name: "javascript",
+		icon: "icons/js.svg",
+	},
+	{
+		name: "html",
+		icon: "icons/html.svg",
+	},
+	{
+		name: "css",
+		icon: "icons/css.svg",
+	},
+	{
+		name: "python",
+		icon: "icons/python.svg",
+	},
+	{
+		name: "c++",
+		icon: "icons/cpp.svg",
+	},
+	{
+		name: "java",
+		icon: "icons/java.svg",
+	},
+	{
+		name: "ruby",
+		icon: "icons/ruby.svg",
+	},
+	{
+		name: "typescript",
+		icon: "icons/typescript.svg",
+	},
+	{
+		name: "json",
+		icon: "icons/json.svg",
+	},
+];

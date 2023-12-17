1a176480420eb6fe894fab800bcff476117f114d
shouravrahman <shouravrahman006@gmail.com>
2023-12-17 21:06:03 +0600

    chore: setup basic colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        hero: 'url("/hero.png")',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#5c6ac4",
+				secondary: "#ecc94b",
+				background: "#000",
+				text: "#fff",
+				accent: "#5c5bc3",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;

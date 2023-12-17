82d12b31d476a5ebf64b834eb276c1a3bd018531
shouravrahman <shouravrahman006@gmail.com>
2023-12-17 21:10:07 +0600

    feat: editor imported

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
-import LoadMore from "../components/LoadMore";
-import { data } from "./_data";
+"use client";
+import Editor from "@/components/Editor";
+import LanguageSelector from "@/components/LanguageSelector";
+import { languages } from "@/utils/utilities";
+import { useState } from "react";
 
-async function Home() {
-  return (
-    <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-      <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
+function Home() {
+	const [language, setLanguage] = useState(languages[0].name);
+	return (
+		<main className=' bg-background'>
+			<header>
+				<LanguageSelector />
+			</header>
 
-      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
-      </section>
-      <LoadMore />
-    </main>
-  );
+			<div className='flex items-center justify-center w-full'>
+				<Editor language={language} />
+			</div>
+		</main>
+	);
 }
 
 export default Home;
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,9 +1,65 @@
+"use client";
 import React from "react";
+import { Resizable } from "re-resizable";
+import AceEditor from "react-ace";
 
-type Props = {};
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-json";
 
-const Editor = (props: Props) => {
-	return <div>Editor</div>;
+import "ace-builds/src-noconflict/ext-language_tools";
+
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-cobalt";
+import "ace-builds/src-noconflict/theme-terminal";
+import "ace-builds/src-noconflict/theme-twilight";
+
+interface EditorProps {
+	onCodeChange: (code: string) => void;
+	language: string;
+	theme: string;
+	icon: string;
+	background?: string;
+	currentPadding?: string;
+}
+
+const Editor = ({
+	onCodeChange,
+	icon,
+	language,
+	background,
+	currentPadding,
+	theme,
+}: EditorProps) => {
+	return (
+		<Resizable
+			className='bg-white'
+			minHeight={510}
+			minWidth={510}
+			maxWidth={1000}
+		>
+			<div className='w-full'>
+				<AceEditor
+					value="function() => {function() { return 'Hello from snappy'} }"
+					fontSize={16}
+					theme='monokai'
+					mode={language.toLowerCase()}
+					name='UNIQUE'
+					showGutter={false}
+					wrapEnabled
+					showPrintMargin={false}
+					highlightActiveLine={false}
+					editorProps={{ $blockScrolling: true }}
+					className='w-full'
+				/>
+			</div>
+		</Resizable>
+	);
 };
 
 export default Editor;

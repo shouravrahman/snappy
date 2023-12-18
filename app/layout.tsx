1d964c33c97989b1ec2163a6c039c76863f1992c
shouravrahman <shouravrahman006@gmail.com>
2023-12-19 01:36:51 +0600

    chore: font and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,25 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
-
-
 import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Snappy",
-  description: "Create beautiful code snapshots.",
+	title: "Snappy",
+	description: "Create beautiful code snapshots.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={dmSans.className}>
-        <main className="max-w-7xl mx-auto bg-[#0F1117]">
-
-          {children}
-
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={dmSans.className}>
+				<main className='max-w-7xl mx-auto'>{children}</main>
+			</body>
+		</html>
+	);
 }

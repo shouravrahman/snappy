049fc47ff23d66fd0f88f8cf91dd8d53fd13d817
shouravrahman <shouravrahman006@gmail.com>
2023-12-20 04:26:48 +0600

    initial code data

--- utils/utilities.tsx
+++ utils/utilities.tsx
@@ -39,5 +39,6 @@ export const backgrounds = [
 	"linear-gradient(to right, #ff7e5f, #feb47b)",
 	"linear-gradient(to right, #74ebd5, #acb6e5)",
 	"linear-gradient(to right, #7d5fff, #86e6cc)",
+	"linear-gradient(45deg, #111518 0%, #012151 100%)",
 ];
-export const initialCode = `const [height, setHeight] = useState(500);`;
+export const initialCode = `paste your code here`;

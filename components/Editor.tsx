279d52f27382d4d98ec807863281281b931bc4cd
shouravrahman <shouravrahman006@gmail.com>
2023-12-20 04:24:45 +0600

    chore: dynamic imports ...

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,23 +3,24 @@ import React, { useState, useEffect } from "react";
 import { Resizable } from "re-resizable";
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/mode-typescript";
-import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/mode-css";
-import "ace-builds/src-noconflict/mode-json";
+// Dynamically import only the needed Ace Editor modes and themes
+const importAceBuilds = async () => {
+	await import("ace-builds/src-noconflict/ext-language_tools");
+	await import("ace-builds/src-noconflict/theme-github");
+	await import("ace-builds/src-noconflict/theme-monokai");
+	await import("ace-builds/src-noconflict/mode-javascript");
+	await import("ace-builds/src-noconflict/mode-python");
+	await import("ace-builds/src-noconflict/mode-java");
+	await import("ace-builds/src-noconflict/mode-typescript");
+	await import("ace-builds/src-noconflict/mode-html");
+	await import("ace-builds/src-noconflict/mode-css");
+	await import("ace-builds/src-noconflict/mode-json");
 
-import "ace-builds/src-noconflict/ext-language_tools";
-
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/theme-cobalt";
-import "ace-builds/src-noconflict/theme-terminal";
-import "ace-builds/src-noconflict/theme-twilight";
-import Image from "next/image";
-import { initialCode } from "@/utils/utilities";
+	await import("ace-builds/src-noconflict/theme-cobalt");
+	await import("ace-builds/src-noconflict/theme-terminal");
+	await import("ace-builds/src-noconflict/theme-twilight");
+	// Import other themes and modes as needed
+};
 
 interface EditorProps {
 	language: string;
@@ -29,21 +30,23 @@ interface EditorProps {
 	currentPadding?: string;
 }
 
-const Editor = ({
+const Editor: React.FC<EditorProps> = ({
 	icon,
 	language,
 	background,
 	currentPadding,
 	theme,
 }: EditorProps) => {
-	const [width, setWidth] = useState(1000);
-	const [height, setHeight] = useState(500);
-	const [title, setTitle] = useState("Untitled-1");
-	const [code, setCode] = useState(initialCode);
+	const [width, setWidth] = useState<number>(1100);
+	const [height, setHeight] = useState<number>(500);
+	const [title, setTitle] = useState<string>("Untitled-1");
+	const [code, setCode] = useState<string>("");
 
-	const handleCodeChange = () => {};
+	const handleCodeChange = (newCode: string) => {
+		setCode(newCode);
+	};
 
-	const handleResize = (e: any, direction: any, ref: any, pos: any) => {
+	const handleResize = (_: any, __: any, ref: any) => {
 		const newHeight = ref.style.height;
 		setHeight(parseInt(newHeight, 10));
 	};
@@ -53,32 +56,30 @@ const Editor = ({
 	};
 
 	useEffect(() => {
-		window.addEventListener("resize", updateSize);
 		updateSize();
+		window.addEventListener("resize", updateSize);
+		// Dynamically import Ace Editor builds
+		importAceBuilds();
 		return () => window.removeEventListener("resize", updateSize);
 	}, []);
 
 	return (
 		<Resizable
-			minHeight={510}
-			minWidth={510}
-			maxWidth={1000}
-			defaultSize={{
-				width: width,
-				height: height,
-			}}
+			maxWidth={1300}
+			minHeight={500}
+			defaultSize={{ width, height }}
 			onResize={handleResize}
-			className='rounded '
-			style={{ background: background, padding: currentPadding }}
+			className='rounded'
+			style={{ background, padding: currentPadding }}
 		>
-			<div className='code-block '>
+			<div className='code-block'>
 				<div className='code-title h-[60px] px-4 flex items-center justify-between bg-black bg-opacity-80'>
 					<div className='flex items-center p-4 gap-1'>
 						<div className='w-3 h-3 rounded-full bg-[#ff5656]'></div>
 						<div className='w-3 h-3 rounded-full bg-[#ff5656]'></div>
 						<div className='w-3 h-3 rounded-full bg-[#ff5656]'></div>
 					</div>
-					<div className='input w-full'>
+					<div>
 						<input
 							type='text'
 							className='w-full text-white outline-none font-medium text-center bg-transparent p-2'
@@ -86,9 +87,9 @@ const Editor = ({
 							onChange={(e) => setTitle(e.target.value)}
 						/>
 					</div>
-					<div className='icon flex justify-center items-center bg-white  rounded-full p-1'>
+					<div className='icon flex justify-center items-center bg-white rounded-full p-1'>
 						{/* icon */}
-						<Image src={icon} width={25} height={25} alt='icon' />
+						<img src={icon} width={25} height={25} alt='icon' />
 					</div>
 				</div>
 				<AceEditor
@@ -98,13 +99,14 @@ const Editor = ({
 					mode={language.toLowerCase()}
 					name='UNIQUE_ID_OF_DIV'
 					showGutter={false}
+					width='100%'
 					height={`calc(${height}px - ${currentPadding} - ${currentPadding} - 60px)`}
 					wrapEnabled
 					showPrintMargin={false}
 					highlightActiveLine={false}
 					editorProps={{ $blockScrolling: true }}
-					className='ace-editor'
 					onChange={handleCodeChange}
+					focus
 				/>
 			</div>
 		</Resizable>

e8baea24f489b06a9f1f60b46ea41fcd41d864dc
shouravrahman <shouravrahman006@gmail.com>
2023-12-20 04:23:48 +0600

    feat: logo,subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
 "use client";
-import BackgroundSelector from "@/components/BackgroundSelector";
-import Editor from "@/components/Editor";
+// import Editor from "@/components/Editor";
 import Footer from "@/components/Footer";
-import LanguageSelector from "@/components/LanguageSelector";
-import PaddingSelector from "@/components/PaddingSelector";
-import ThemeSelector from "@/components/ThemeSelector";
 import { backgrounds, languages, themes } from "@/utils/utilities";
-import { Download } from "lucide-react";
 import { useRef, useState } from "react";
 import html2canvas from "html2canvas";
 import Header from "@/components/Header";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+const Editor = dynamic(() => import("@/components/Editor"), {
+	ssr: false,
+	loading: () => <div>Loading Editor...</div>,
+});
 
 function Home() {
 	const editorRef = useRef(null);
 
 	const [language, setLanguage] = useState(languages[0].name);
-	const [theme, setTheme] = useState(themes[0]);
-	const [bg, setBg] = useState(backgrounds[1]);
+	const [theme, setTheme] = useState(themes[1]);
+	const [bg, setBg] = useState(backgrounds[3]);
 	const [padding, setPadding] = useState(["1rem", "2rem", "3rem"]);
 	const [currentPadding, setCureentPadding] = useState(padding[2]);
 	const [activeIcon, setActiveIcon] = useState(languages[0].icon);
@@ -38,8 +40,19 @@ function Home() {
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-between sm:px-4'>
-			<h1 className='text-6xl text-yellow-400 mt-10 p-2'>Snappy!</h1>
+		<div className='flex flex-col items-center justify-between px-4 mt-10'>
+			{/* <h1 className='text-6xl text-yellow-400 mt-12 p-2'>Snappy!</h1> */}
+			<Image
+				src='/logo-trs.png'
+				alt='logo'
+				width={100}
+				height={0}
+				className='w-40 h-28 object-cover'
+			/>
+			<h2 className={`text-xl max-w-4xl text-center text-white mb-6`}>
+				Create and share beautiful images of your source code. Start typing or
+				paste into the text area to get started.
+			</h2>
 			<Header
 				language={language}
 				setLanguage={setLanguage}
@@ -54,7 +67,7 @@ function Home() {
 				exportPng={exportPng}
 			/>
 
-			<div className='mt-10 w-full h-full mx-auto m-2' ref={editorRef}>
+			<div className='mt-10 flex justify-center text-white' ref={editorRef}>
 				<Editor
 					background={bg}
 					language={language}

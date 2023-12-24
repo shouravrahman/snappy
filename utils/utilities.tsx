4039ea4342c50d1996f2b118e2608e975f0616aa
shouravrahman <shouravrahman006@gmail.com>
2023-12-24 23:03:08 +0600

    updated

--- utils/utilities.tsx
+++ utils/utilities.tsx
@@ -4,6 +4,10 @@ export const languages = [
 		icon: "icons/js.svg",
 	},
 	{
+		name: "JSX",
+		icon: "icons/jsx.svg",
+	},
+	{
 		name: "Html",
 		icon: "icons/html.svg",
 	},
@@ -32,13 +36,45 @@ export const languages = [
 		name: "JSON",
 		icon: "icons/json.svg",
 	},
+	{
+		name: "SQL",
+		icon: "icons/sql.svg",
+	},
 ];
 
-export const themes = ["monokai", "cobalt", "terminal"];
+export const themes = [
+	"monokai",
+	"cobalt",
+	"terminal",
+	"twilight",
+	"chaos",
+	"cloud9_night",
+	"crimson_editor",
+	"dracula",
+];
+
 export const backgrounds = [
 	"linear-gradient(to right, #ff7e5f, #feb47b)",
 	"linear-gradient(to right, #74ebd5, #acb6e5)",
 	"linear-gradient(to right, #7d5fff, #86e6cc)",
 	"linear-gradient(45deg, #111518 0%, #012151 100%)",
+	"linear-gradient(0deg, hsl(0, 16%, 7%) 0%, hsl(348, 13%, 8%) 1%, hsl(335, 11%, 8%) 3%, hsl(318, 8%, 9%) 5%, hsl(292, 6%, 9%) 7%, hsl(264, 6%, 9%) 10%, hsl(240, 6%, 10%) 13%, hsl(240, 5%, 10%) 16%, hsl(240, 5%, 11%) 20%, hsl(240, 5%, 11%) 24%, hsl(240, 4%, 11%) 29%, hsl(240, 4%, 11%) 35%, hsl(240, 3%, 12%) 43%, hsl(264, 3%, 11%) 51%, hsl(291, 2%, 10%) 62%, hsl(319, 3%, 9%) 73%, hsl(336, 3%, 9%) 84%, hsl(349, 3%, 8%) 93%, hsl(0, 3%, 7%) 100%)",
 ];
-export const initialCode = `paste your code here`;
+export const initialCode = [
+	`
+    const emptyPromise = new Promise(resolve => 
+        resolve("This promise is as empty as my coffee cup.")
+    );
+    `,
+	`
+    document.addEventListener("click", event => {
+        alert("Ouch! That hurt my feelings.");
+    });
+    `,
+	`
+    const myLife = {
+        status: "under construction",
+        happiness: "coming soon",
+    };
+    `,
+];

f81a37c4b19fae5d60a344d1cf5d9d22970dbcb9
shouravrahman <shouravrahman006@gmail.com>
2023-12-24 23:04:04 +0600

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,19 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className='flex text-white items-center gap-10 py-12'>
+		<footer className='flex text-white items-center gap-6 py-12'>
 			<Link
 				href='https://www.linkedin.com/in/shouravrahman'
 				target='_blank'
 				className='text-lg '
 			>
-				Made by <strong className='text-yellow-400'>shouravrahman</strong>
+				Made with 🧠 by{" "}
+				<strong className='text-yellow-400'>shouravrahman</strong>
 			</Link>
 			<Link
 				href='https://github.com/shouravrahman/snappy'
 				target='_blank'
-				className='text-sm font-medium'
+				className='text-sm font-medium ml-4'
 			>
 				<Github />
 			</Link>

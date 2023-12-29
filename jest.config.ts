52bfaf11386fe089f6b1b4d2c083c4951cd622c7
shouravrahman <shouravrahman006@gmail.com>
2023-12-29 07:00:28 +0600

    chore: setup testing

---
+++ jest.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  preset: 'ts-jest',
+
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)

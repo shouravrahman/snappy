6ca93725a955af7fd26db10f71eb8d7dd8a84551
shouravrahman <shouravrahman006@gmail.com>
2023-12-31 00:25:04 +0600

    added cool backgrounds

--- utils/utilities.tsx
+++ utils/utilities.tsx
@@ -54,12 +54,107 @@ export const themes = [
 ];
 
 export const backgrounds = [
-	"linear-gradient(to right, #ff7e5f, #feb47b)",
-	"linear-gradient(to right, #74ebd5, #acb6e5)",
-	"linear-gradient(to right, #7d5fff, #86e6cc)",
-	"linear-gradient(45deg, #111518 0%, #012151 100%)",
-	"linear-gradient(0deg, hsl(0, 16%, 7%) 0%, hsl(348, 13%, 8%) 1%, hsl(335, 11%, 8%) 3%, hsl(318, 8%, 9%) 5%, hsl(292, 6%, 9%) 7%, hsl(264, 6%, 9%) 10%, hsl(240, 6%, 10%) 13%, hsl(240, 5%, 10%) 16%, hsl(240, 5%, 11%) 20%, hsl(240, 5%, 11%) 24%, hsl(240, 4%, 11%) 29%, hsl(240, 4%, 11%) 35%, hsl(240, 3%, 12%) 43%, hsl(264, 3%, 11%) 51%, hsl(291, 2%, 10%) 62%, hsl(319, 3%, 9%) 73%, hsl(336, 3%, 9%) 84%, hsl(349, 3%, 8%) 93%, hsl(0, 3%, 7%) 100%)",
+	{ name: "Cosmic Dust", value: "linear-gradient(to right, #a0c4ff, #4a69ff)" },
+	{
+		name: "Galactic Glow",
+		value: "linear-gradient(to bottom, #192a56, #25387d)",
+	},
+	{
+		name: "Nebula Burst",
+		value: "radial-gradient(circle, #4a4582 0%, #7575a3 45%, #5d5d8d 100%)",
+	},
+	{
+		name: "Polaris Flare",
+		value: "linear-gradient(45deg, #ebcc85 0%, #547595 100%)",
+	},
+	{
+		name: "Interstellar Mist",
+		value: "linear-gradient(to right, #b3d4ff, #85a5ff)",
+	},
+	{
+		name: "Supernova Shimmer",
+		value: "radial-gradient(circle, #ffbb99 0%, #f58231 45%, #ffbb99 100%)",
+	},
+	{
+		name: "Celestial Aura",
+		value: "radial-gradient(circle, #354a60 0%, #506575 45%, #546978 100%)",
+	},
+	{
+		name: "Meteor Shower",
+		value: "linear-gradient(45deg, #a9c5f1 0%, #5578c1 100%)",
+	},
+	{ name: "Dark Nebula", value: "linear-gradient(to top, #354a60, #4a657a)" },
+	{
+		name: "Starlight Path",
+		value: "linear-gradient(to right, #54a7ff, #3a86ff)",
+	},
+	,
+	{
+		name: "Melon Splash",
+		value: "linear-gradient(to right, #ff726f, #f9d4a5)",
+	},
+	{
+		name: "Tropical Delight",
+		value: "linear-gradient(to bottom, #a2f5d5, #5de5fa)",
+	},
+	{
+		name: "Lavender Fields",
+		value: "linear-gradient(to top, #a891f3, #e5ceff)",
+	},
+	{
+		name: "Sunset Sorbet",
+		value: "linear-gradient(to right, #ff5f54, #f0988e)",
+	},
+	{
+		name: "Stardust",
+		value:
+			"linear-gradient(45deg, #2bffea 20%, #0054a6 80%), repeating-linear-gradient(-45deg, rgba(255,255,255,.1) 5px, transparent 5px) 0 0/5px 5px",
+	},
+	{
+		name: "Moonlit Water",
+		value:
+			"linear-gradient(to bottom, #2c3e50, #457b9d), radial-gradient(circle at bottom, #004a85, #00254a)",
+	},
+	{
+		name: "Solar Flare",
+		value:
+			"radial-gradient(circle at top, #f5ba95 0%, #ee5455 50%, #f5ba95 100%)",
+	},
+	{
+		name: "Cyberpunk Dreams",
+		value: "linear-gradient(to right, #3a506b, #a052c9)",
+	},
+	{
+		name: "Neon Jungle",
+		value: "linear-gradient(to right, #45f542, #2bffa5)",
+	},
+	{
+		name: "Abstract Waves",
+		value:
+			"linear-gradient(to right, #ff54f3, #f5478d), linear-gradient(to bottom, #654ea3, #a454f3) 40% 50%/50% 50%",
+	},
+	{
+		name: "Arctic Aurora",
+		value: "linear-gradient(to right, #7f5af0, #e5bfff)",
+	},
+	{
+		name: "Pink Sunrise",
+		value: "linear-gradient(to right, #f78da7, #f54785)",
+	},
+	{
+		name: "Deep Sea",
+		value: "linear-gradient(to bottom, #2980b9, #4fc3f7)",
+	},
+	{
+		name: "Cyberpunk City",
+		value: "linear-gradient(to right, #54a4f3, #a254f3)",
+	},
+	{
+		name: "Neon Lights",
+		value: "linear-gradient(to right, #f05454, #ffcc54)",
+	},
 ];
+
 export const initialCode = [
 	`
     const emptyPromise = new Promise(resolve => 
